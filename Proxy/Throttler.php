8ff93ab3588b4ac98818ba416f520084806a06d7
XiangMin Liu <635750556@qq.com>
2019-05-08 22:23:49 +0800

    refactor(facade): rename facade to proxy

--- Proxy/Throttler.php
+++ Proxy/Throttler.php
@@ -18,12 +18,12 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Leevel\Throttler\Facade;
+namespace Leevel\Throttler\Proxy;
 
 use Leevel\Di\Container;
 
 /**
- * 门面 throttler.
+ * 代理 throttler.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
